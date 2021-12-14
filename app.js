fbab6656594427354f6de451cfaa4d9cac37f29f
michael-lugashi <michael.lugashi@gmail.com>
2021-12-14 08:12:14 +0200

    inserted request handling into blog controller

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const notesRouter = require('./controllers/notes');
+const blogRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-const Blog = require('./model/blogSchema');
 
 logger.info('connecting to', config.MONGODB_URI);
 
@@ -27,23 +26,10 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-// app.use('/api/notes', notesRouter);
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
